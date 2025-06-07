bb2d38ca6c6801f90dd7338e9245fb6f174df6b4
fetasty <fetasty@outlook.com>
2025-06-07 19:20:32 +0800

    feat: Hud buff item flashing

--- hud/buff_item.gd
+++ hud/buff_item.gd
@@ -4,6 +4,10 @@ extends CenterContainer
 ## Must init before add to scenetree
 var buff_res: Buff
 
+## Temporary fields
+var flash_tween: Tween = null
+var is_flashing: bool = false
+
 @onready var buff_container: Control = %BuffContainer
 @onready var buff_texture: TextureRect = %BuffTexture
 @onready var time_label: Label = %TimeLabel
@@ -16,17 +20,34 @@ func _ready() -> void:
 	buff_container.custom_minimum_size = buff_texture.size * buff_texture.scale.x
 
 
+func set_flashing(flashing: bool) -> void:
+	if flashing and flash_tween == null:
+		is_flashing = true
+		flash_tween = get_tree().create_tween()
+		flash_tween.tween_property(buff_texture, "modulate", Color.TRANSPARENT, 0.25)
+		flash_tween.tween_property(buff_texture, "modulate", Color.WHITE, 0.25)
+		flash_tween.set_loops()
+		flash_tween.play()
+	elif not flashing and flash_tween != null:
+		is_flashing = false
+		flash_tween.stop()
+		flash_tween = null
+		buff_texture.modulate = Color.WHITE
+
+
 func on_data_changed(key: StringName, value: Variant) -> void:
 	match key:
 		Const.BUFF:
 			var id = value["id"]
 			var time = value["time"]
 			if id == buff_res.id:
-				if time > 1.0:
-					time_label.text = "%ds" % int(time)
-				else:
-					time_label.text = "%.2fs" % time
+				time_label.text = "%.1fs" % time
+				if is_flashing and time >= 2.0:
+					set_flashing(false)
+				if not is_flashing and time > 0.0 and time < 2.0:
+					set_flashing(true)
 				if time <= 0.0:
+					set_flashing(false)
 					visible = false
 				elif not visible:
 					visible = true
